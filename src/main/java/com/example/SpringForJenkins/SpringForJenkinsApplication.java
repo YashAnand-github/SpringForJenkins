bcbd3f6560fbb59d92c8a5b38ba6bcfeed7f660b
Yash <66507542+YashAnand-github@users.noreply.github.com>
2025-04-30 23:01:58 +0530

    Add files via upload
    first commit

---
+++ src/main/java/com/example/SpringForJenkins/SpringForJenkinsApplication.java
@@ -0,0 +1,30 @@
+package com.example.SpringForJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/spring-boot")
+@RestController
+@SpringBootApplication
+public class SpringForJenkinsApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringForJenkinsApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringForJenkinsApplication.class);
+    }
+
+    @GetMapping("/hello")
+    public String firstApp(@RequestParam(value = "name", defaultValue = "Coder") String name) {
+        return String.format("Congratulations  %s, you've built your first Spring Boot Application from Scratch!!", name);
+    }
+}
---
+++ src/test/java/com/example/SpringForJenkins/SpringForJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringForJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringForJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
